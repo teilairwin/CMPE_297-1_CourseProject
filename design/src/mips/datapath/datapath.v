b94fa8aed1380e8f8352a9b03467ddb4b0ca5d52
bleufred <kayleemiller17@gmail.com>
2021-03-10 18:52:52 -0700

    MIPs with four factorial base update

--- design/src/mips/datapath/datapath.v
+++ design/src/mips/datapath/datapath.v
@@ -213,3 +213,227 @@ module datapath (
         );
 
 endmodule
+
+
+
+module datapath4 (
+        input  wire        clk,
+        input  wire        rst,
+        input  wire        branch,
+        input  wire        jump,
+        input  wire        jal,
+        input  wire        jr,
+        input  wire        shmux,
+        input  wire        reg_dst,
+        input  wire        we_reg,
+        input  wire        alu_src,
+        input  wire        dm2reg,
+        input  wire        mult_enable,
+        input  wire        sfmux_high,
+        input  wire        sf2reg,
+        input  wire [3:0]  alu_ctrl,
+        input  wire [4:0]  ra3,
+        input  wire [31:0] instr,
+        input  wire [31:0] rd_dm0,
+        input  wire [31:0] rd_dm1,
+        input  wire [31:0] rd_dm2,
+        input  wire [31:0] rd_dm3,
+        output wire [31:0] pc_current,
+        output wire [31:0] alu_out,
+        output wire [31:0] wd_dm0,
+        output wire [31:0] wd_dm1,
+        output wire [31:0] wd_dm2,
+        output wire [31:0] wd_dm3,
+        output wire [31:0] rd3
+    );
+
+    wire [31:0] shamt_in;
+    wire [4:0]  rf_wa;
+    wire [4:0]  rf_wa_rdt;
+    wire [4:0]  rf_wa_ra;
+    wire        pc_src;
+    wire [31:0] pc_plus4;
+    wire [31:0] pc_pre;
+    wire [31:0] pc_next;
+    wire [31:0] pc_next_pre;
+    wire [31:0] sext_imm;
+    wire [31:0] ba;
+    wire [31:0] bta;
+    wire [31:0] jta;
+    wire [31:0] alu_pa;
+    wire [31:0] alu_pb;
+    wire [31:0] wd_rf;
+    wire [31:0] high_in;
+    wire [31:0] low_in;
+    wire [31:0] sfmux_in0;
+    wire [31:0] sfmux_in1;
+    wire [31:0] sfmux_out;
+    wire [31:0] wd_aludm;
+    wire [31:0] wd_sfhl;
+    wire [31:0] rs;
+
+    wire        zero;
+
+    assign pc_src = branch & zero;
+    assign ba = {sext_imm[29:0], 2'b00};
+    assign jta = {pc_plus4[31:28], instr[25:0], 2'b00};
+    assign shamt_in = {27'd0, instr[10:6]};
+
+    // --- PC Logic --- //
+    dreg pc_reg (
+            .clk            (clk),
+            .rst            (rst),
+            .d              (pc_next),
+            .q              (pc_current)
+        );
+
+    adder pc_plus_4 (
+            .a              (pc_current),
+            .b              (32'd4),
+            .y              (pc_plus4)
+        );
+
+    adder pc_plus_br (
+            .a              (pc_plus4),
+            .b              (ba),
+            .y              (bta)
+        );
+
+    mux2 #(32) pc_src_mux (
+            .sel            (pc_src),
+            .a              (pc_plus4),
+            .b              (bta),
+            .y              (pc_pre)
+        );
+
+    mux2 #(32) pc_jmp_mux (
+            .sel            (jump),
+            .a              (pc_pre),
+            .b              (jta),
+            .y              (pc_next_pre)
+        );
+
+    // --- jal & jr Support --- //
+    mux2 #(32) rfwd_jal_mux (
+            .sel            (jal),
+            .a              (wd_sfhl),
+            .b              (pc_plus4),
+            .y              (wd_rf)
+    );
+
+    mux2 #(32)  rfwa_jal_mux (
+            .sel            (jal),
+            .a              (rf_wa_rdt),
+            .b              (5'd31),
+            .y              (rf_wa_ra)
+    );
+
+    mux2 #(32)  pcjr_mux (
+            .sel            (jr),
+            .a              (pc_next_pre),
+            .b              (rs),
+            .y              (pc_next)
+    );
+
+    mux2 #(5)   rfjr_mux (
+            .sel            (jr),
+            .a              (rf_wa_ra),
+            .b              (5'd0),
+            .y              (rf_wa)
+    );
+
+    // --- sll, srl Support --- //
+    mux2 #(32) shift_mux (
+            .sel            (shmux),
+            .a              (rs),
+            .b              (shamt_in),
+            .y              (alu_pa)
+    );
+
+    // --- RF Logic --- //
+    mux2 #(5) rf_wa_mux (
+            .sel            (reg_dst),
+            .a              (instr[20:16]),
+            .b              (instr[15:11]),
+            .y              (rf_wa_rdt)
+        );
+
+    regfile rf (
+            .clk            (clk),
+            .we             (we_reg),
+            .ra1            (instr[25:21]),
+            .ra2            (instr[20:16]),
+            .ra3            (ra3),
+            .wa             (rf_wa),
+            .wd             (wd_rf),
+            .rd1            (rs),
+            .rd2            (wd_dm),
+            .rd3            (rd3)
+        );
+
+    signext se (
+            .a              (instr[15:0]),
+            .y              (sext_imm)
+        );
+
+    // --- ALU Logic --- //
+    mux2 #(32) alu_pb_mux (
+            .sel            (alu_src),
+            .a              (wd_dm),
+            .b              (sext_imm),
+            .y              (alu_pb)
+        );
+
+    alu alu (
+            .op             (alu_ctrl),
+            .a              (alu_pa),
+            .b              (alu_pb),
+            .zero           (zero),
+            .y              (alu_out)
+        );
+
+    // --- MULTIPLIER --- //
+    multiplier mult(
+            .A            (rs),
+            .B            (wd_dm),
+            .en           (mult_enable),
+            .Y            ({high_in, low_in})
+    );
+
+    dreg low_reg(
+            .clk          (clk),
+            .rst          (rst),
+            .d            (low_in),
+            .q            (sfmux_in0)
+    );
+
+    dreg high_reg(
+            .clk          (clk),
+            .rst          (rst),
+            .d            (high_in),
+            .q            (sfmux_in1)
+    );
+
+    mux2 #(32) sfmux (
+            .sel          (sfmux_high),
+            .a            (sfmux_in0),
+            .b            (sfmux_in1),
+            .y            (sfmux_out)
+    );
+
+    mux2 #(32) rf_sfhl_mux (
+            .sel          (sf2reg),
+            .a            (wd_aludm),
+            .b            (sfmux_out),
+            .y            (wd_sfhl)
+    );
+
+    // --- MEM Logic --- //
+    mux2 #(32) rf_wd_mux (
+            .sel            (dm2reg),
+            .a              (alu_out),
+            .b              (rd_dm),
+            .y              (wd_aludm)
+        );
+
+endmodule
--- design/src/soc/system.v
+++ design/src/soc/system.v
@@ -11,37 +11,64 @@ module system (
         output wire [31:0] pc_current,
         output wire [31:0] instr,
         output wire [31:0] alu_out,
-        output wire [31:0] wd_mm,
-        output wire [31:0] rd_mm,
+        output wire [31:0] wd_mm0,
+        output wire [31:0] wd_mm1,
+        output wire [31:0] wd_mm2,
+        output wire [31:0] wd_mm3,
+        output wire [31:0] rd_mm0,
+        output wire [31:0] rd_mm1,
+        output wire [31:0] rd_mm2,
+        output wire [31:0] rd_mm3,
         output wire [31:0] rd3,
         // fix for multM_reg/q_reg
         output wire multM_reg_fixme
     );
 
-    wire [31:0] DMemData, FactData0, FactData1, FactData2, FactData3, GPIOData, ReadData, rd_dm;
-    wire        WE1, WE2, WEM, we_dm;
+    wire [31:0] DMemData0, DMemData1, DMemData2, DMemData3;
+    wire [31:0] FactData0, FactData1, FactData2, FactData3;
+    wire [31:0] GPIOData0, GPIOData1, GPIOData2, GPIOData3;
+    wire [31:0] ReadData0, ReadData1, ReadData2, ReadData3;
+    wire [31:0] rd_dm0, rd_dm1, rd_dm2, rd_dm3;
+    wire        WE1, WE2;
+    wire        WEM0, WEM1, WEM2, WEM3;
+    wire        we_dm0, we_dm1, we_dm2, we_dm3;
     wire [ 1:0] RdSel;
     wire [ 3:0] Done;             // Using vector for Done signals
     wire        iack, irq, addr;
 
-    assign rd_mm = ReadData;
-    assign DMemData = rd_dm;
-    assign we_dm = WEM;
+    assign rd_mm0 = ReadData0;
+    assign rd_mm1 = ReadData1;
+    assign rd_mm2 = ReadData2;
+    assign rd_mm3 = ReadData3;
+    assign DMemData0 = rd_dm0;
+    assign DMemData1 = rd_dm1;
+    assign DMemData2 = rd_dm2;
+    assign DMemData3 = rd_dm3;
+    assign we_dm0 = WEM0;
+    assign we_dm1 = WEM1;
+    assign we_dm2 = WEM2;
+    assign we_dm3 = WEM3;
 
     // -- assignment_3/mips_top
     // note: some _dm signals have been replaced with _mm signals
     wire [31:0] DONT_USE;
 
-    mips mips (
+    mips4 mips4 (
         .clk          (clk),
         .rst          (rst),
         .ra3          (ra3),
         .instr        (instr),
-        .rd_dm        (rd_mm),
+        .rd_dm0       (rd_mm0),
+        .rd_dm1       (rd_mm1),
+        .rd_dm2       (rd_mm2),
+        .rd_dm3       (rd_mm3),
         .we_dm        (we_mm),
         .pc_current   (pc_current),
         .alu_out      (alu_out),
-        .wd_dm        (wd_mm),
+        .wd_dm0       (wd_mm0),
+        .wd_dm1       (wd_mm1),
+        .wd_dm2       (wd_mm2),
+        .wd_dm3       (wd_mm3),
         .rd3          (rd3)
     );
 
@@ -56,21 +83,30 @@ module system (
         // .y            (instr)
     // );
 
-    dmem dmem (
+    dmem4 dmem4 (
         .clk          (clk),
         .we           (we_dm),
         .a            (alu_out[7:2]),
-        .d            (wd_mm),
-        .q            (rd_dm)
+        .d0           (wd_mm0),
+        .d1           (wd_mm1),
+        .d2           (wd_mm2),
+        .d3           (wd_mm3),
+        .q0           (rd_dm0),
+        .q1           (rd_dm1),
+        .q2           (rd_dm2),
+        .q3           (rd_dm3)
     );
     // -- assignment_3/mips_top
 
-    address_decoder addr_decoder(
+    address_decoder addr_decoder4(
         .WE           (we_mm),
         .A            ({20'b0, alu_out[11:4], 4'b0}),
         .WE2          (WE2),
         .WE1          (WE1),
-        .WEM          (WEM),
+        .WEM0         (WEM0),
+        .WEM1         (WEM1),
+        .WEM2         (WEM2),
+        .WEM3         (WEM3),
         .RdSel        (RdSel)
     );
 
@@ -80,7 +116,7 @@ module system (
         .rst          (rst),
         .A            (alu_out[3:2]),   // Todo - Different signal?
         .WE           (WE1),            // Todo - Different signal?
-        .WD           (wd_mm),          // Todo - Different signal?
+        .WD           (wd_mm0),         // Todo - Different signal?
         .RD           (FactData0),
         .Done         (Done[0])
     );
@@ -91,7 +127,7 @@ module system (
         .rst          (rst),
         .A            (alu_out[3:2]),   // Todo - Different signal?
         .WE           (WE1),            // Todo - Different signal?
-        .WD           (wd_mm),          // Todo - Different signal?
+        .WD           (wd_mm1),         // Todo - Different signal?
         .RD           (FactData1),
         .Done         (Done[1])
     );
@@ -102,7 +138,7 @@ module system (
         .rst          (rst),
         .A            (alu_out[3:2]),   // Todo - Different signal?
         .WE           (WE1),            // Todo - Different signal?
-        .WD           (wd_mm),          // Todo - Different signal?
+        .WD           (wd_mm2),         // Todo - Different signal?
         .RD           (FactData2),
         .Done         (Done[2])
     );
@@ -113,31 +149,64 @@ module system (
         .rst          (rst),
         .A            (alu_out[3:2]),   // Todo - Different signal?
         .WE           (WE1),            // Todo - Different signal?
-        .WD           (wd_mm),          // Todo - Different signal?
+        .WD           (wd_mm3),         // Todo - Different signal?
         .RD           (FactData3),
         .Done         (Done[3])
     );
 
-    gpio_top gpio_top(
+    gpio_top4 gpio_top4(
         .clk          (clk),
         .rst          (rst),
         .A            (alu_out[3:2]),
         .WE           (WE2),
-        .WD           (wd_mm),
-        .RD           (GPIOData),
+        .WD0          (wd_mm0),
+        .WD1          (wd_mm1),
+        .WD2          (wd_mm2),
+        .WD3          (wd_mm3),
+        .RD0          (GPIOData0),
+        .RD1          (GPIOData1),
+        .RD2          (GPIOData2),
+        .RD3          (GPIOData3),
         .gpI1         (gpI1),
         .gpI2         (gpI2),
         .gpO1         (gpO1),
         .gpO2         (gpO2)
     );
 
-    mux4 mux4(
+    mux4 mux4_0(
         .sel          (RdSel),
-        .a            (DMemData),
-        .b            (DMemData),
-        .c            (FactData),
-        .d            (GPIOData),
-        .y            (ReadData)
+        .a            (DMemData0),
+        .b            (DMemData0),
+        .c            (FactData0),
+        .d            (GPIOData0),
+        .y            (ReadData0)
+    );
+    
+    mux4 mux4_1(
+        .sel          (RdSel),
+        .a            (DMemData1),
+        .b            (DMemData1),
+        .c            (FactData1),
+        .d            (GPIOData1),
+        .y            (ReadData1)
+    );
+    
+    mux4 mux4_2(
+        .sel          (RdSel),
+        .a            (DMemData2),
+        .b            (DMemData2),
+        .c            (FactData2),
+        .d            (GPIOData2),
+        .y            (ReadData2)
+    );
+    
+    mux4 mux4_3(
+        .sel          (RdSel),
+        .a            (DMemData3),
+        .b            (DMemData3),
+        .c            (FactData3),
+        .d            (GPIOData3),
+        .y            (ReadData3)
     );
     
     intc intc(
