d647fd89f4e73ed74f98ab0a70b8917037328935
mkl1ghtn1ng <64824197+mkl1ghtn1ng@users.noreply.github.com>
2021-03-14 15:39:08 -0600

    MIPS-Lite update for exception context switching

--- design/src/mips/datapath/dreg.v
+++ design/src/mips/datapath/dreg.v
@@ -10,3 +10,18 @@ module dreg # (parameter WIDTH = 32) (
         else     q <= d;
     end
 endmodule
+
+module dreg_we # (parameter WIDTH=32) (
+        input wire clk,
+        input wire rst,
+        input wire we,
+        input wire [WIDTH-1:0] d,
+        output reg [WIDTH-1:0] q
+    );
+    
+    always @ (posedge clk, posedge rst) begin
+        if(rst) q <=0;
+        else if(we) q <= d;
+    end
+endmodule
+
