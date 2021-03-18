feb6881b2974ff2d28e17ce11665953769a3b0f7
bleufred <kayleemiller17@gmail.com>
2021-03-17 20:12:16 -0600

    Update Block Diagram 2021-03-15

---
+++ design/src/soc/priority_encoder.v
@@ -0,0 +1,37 @@
+module priority_encoder(
+        input  wire  [3:0] interrupts,
+        output reg   [1:0] y,
+        // we can move the OR gate into the priority encoder if we use a validity flag
+        output reg         valid
+    );
+    always@(interrupts)
+        casex(interrupts)
+            // 0 is highest priority, 3 is lowest, valid is 1 when interrupt 
+            // Priority 0
+            4'bxxx1 : begin
+                      y = 2'b00;
+                      valid = 1;
+                      end
+            // Priority 1
+            4'bxx10 : begin
+                        y = 2'b01;
+                        valid = 1;
+                      end
+            // Priority 2
+            4'bx100 : begin
+                        y = 2'b10;
+                        valid = 1;
+                      end
+            // Priority 3
+            4'b1000 : begin
+                        y = 2'b11;
+                        valid = 1;
+                      end
+            // No interrupt
+            default:  begin 
+                        y = 2'b00;
+                        valid = 0;
+                      end
+        endcase
+        
+endmodule
--- design/src/soc/system.v
+++ design/src/soc/system.v
@@ -1,21 +1,15 @@
 module system (
         input  wire        clk,
         input  wire        rst,
-        // input  wire [31:0] gpI1,
-        // input  wire [31:0] gpI2,
-        // output wire [31:0] gpO1,
-        // output wire [31:0] gpO2,
-        // used for exposing system data sources
         input  wire [4:0]  ra3,
         output wire        we_mm,
         output wire [31:0] pc_current,
-        output wire [31:0] instr,
+        output wire [31:0] instrE,
+        output wire [31:0] instrP,
         output wire [31:0] alu_out,
         output wire [31:0] wd_mm,
         output wire [31:0] rd_mm,
-        output wire [31:0] rd3,
-        // fix for multM_reg/q_reg
-        output wire multM_reg_fixme
+        output wire [31:0] rd3
     );
 
 
@@ -25,16 +19,16 @@ module system (
     wire        WE1, WE2;
     wire        WEM;
     wire        we_dm;
-    wire [ 3:0] Done;             // Using vector for Done signals
+    wire [ 3:0] Done;
     wire [ 2:0] RdSel;
     wire [31:0] instrP, instrE;
     wire [31:0] irq_addr;
-    wire irq_ack, irq; //move to mips output
+    wire irq_ack, irq;
+
     assign rd_mm = ReadData;
     assign DMemData = rd_dm;
     assign we_dm = WEM;
 
-    // -- assignment_3/mips_top
     // note: some _dm signals have been replaced with _mm signals
     wire [31:0] DONT_USE;
 
@@ -55,15 +49,17 @@ module system (
         .irq_addr     (irq_addr)
     );
 
+    // Program Memory
     imem imem (
         .a            (pc_current[7:2]),
         .y            (instrP)
     );
-	
-	imem emem (
+
+    // External Memory
+    imem emem (
         .a            (pc_current[7:2]),
         .y            (instrE)
-	);
+    );
 
     dmem dmem (
         .clk          (clk),
@@ -73,17 +69,52 @@ module system (
         .q            (rd_dm)
     );
 
-    // Swap out Address Decoder with Memory Map
-    address_decoder addr_decoder4(
-        .WE           (we_mm),
-        .A            ({20'b0, alu_out[11:4], 4'b0}),
-        .WE2          (WE2),
-        .WE1          (WE1),
-        .WEM0         (WEM0),
-        .WEM1         (WEM1),
-        .WEM2         (WEM2),
-        .WEM3         (WEM3),
-        .RdSel        (RdSel[1:0])   // Need this to be 3 bits.
+    // // Swap out Address Decoder with Memory Map
+    // address_decoder addr_decoder(
+    //     .WE           (we_mm),
+    //     .A            ({20'b0, alu_out[11:4], 4'b0}),
+    //     .WE2          (WE2),
+    //     .WE1          (WE1),
+    //     .WEM0         (WEM0),
+    //     .WEM1         (WEM1),
+    //     .WEM2         (WEM2),
+    //     .WEM3         (WEM3),
+    //     .RdSel        (RdSel[1:0])   // Need this to be 3 bits.
+    // );
+
+    // mux6 mux6(
+    //     .sel          (RdSel),
+    //     .a            (DMemData),
+    //     .b            (FactData0),
+    //     .c            (FactData1),
+    //     .d            (FactData2),
+    //     .e            (FactData3),
+    //     .f            (irq),        //TBD
+    //     .y            (ReadData)
+    // );
+
+    memory_map_top memory_map_top(
+        write_enable    (write_enable),
+        input_address   (input_address),
+        input_data      (input_data),
+        fact0_data      (fact0_data),
+        fact1_data      (fact1_data),
+        fact2_data      (fact2_data),
+        fact3_data      (fact3_data),
+        fact0_addr      (fact0_addr),
+        fact1_addr      (fact1_addr),
+        fact2_addr      (fact2_addr),
+        fact3_addr      (fact3_addr),
+        dm_we           (dm_we),
+        intc_we         (intc_we),
+        fact0_we        (fact0_we),
+        fact1_we        (fact1_we),
+        fact2_we        (fact2_we),
+        fact3_we        (fact3_we),
+        dm_addr         (dm_addr),
+        intc_addr       (intc_addr),
+        dm_data         (dm_data),
+        intc_data       (intc_data)
     );
 
     // Factorial Unit 0
@@ -128,17 +159,6 @@ module system (
         .WD           (wd_mm3),
         .RD           (FactData3),
         .Done         (Done[3])
-    );
-
-    mux6 mux6(
-        .sel          (RdSel),
-        .a            (DMemData),
-        .b            (FactData0),
-        .c            (FactData1),
-        .d            (FactData2),
-        .d            (FactData3),
-        .d            (Intc),
-        .y            (ReadData)
     );
     
     intc interrupt_controller(
