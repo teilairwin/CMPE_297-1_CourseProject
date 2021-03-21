f0d37c0bef0c00b4cc4ccfb341e0793f51c69d7b
mkl1ghtn1ng <64824197+mkl1ghtn1ng@users.noreply.github.com>
2021-03-21 16:07:23 -0600

    Update driver loader script...

--- scripts/load-driver.tcl
+++ scripts/load-driver.tcl
@@ -1,5 +1,9 @@
 
 cd ../val/src
-exec pscp -P 22 -pw root -r zybo-ui root@zybo:/home/root
+exec pscp -P 22 -pw root -r common root@zybo:/home/root
+exec pscp -P 22 -pw root mainRomLoader.cpp root@zybo:/home/root
+exec pscp -P 22 -pw root exc.bin root@zybo:/home/root
+exec pscp -P 22 -pw root regfile.bin root@zybo:/home/root
+exec pscp -P 22 -pw root buildAll.sh root@zybo:/home/root
 cd ../../scripts
 exec putty.exe -ssh root@zybo -pw root -m make_driver
--- val/src/buildAll.sh
+++ val/src/buildAll.sh
@@ -1,8 +1,8 @@
 
-echo "Building ZYBO-UI"
-cd zybo-ui
-make
-cd ../
+#echo "Building ZYBO-UI"
+#cd zybo-ui
+#make
+#cd ../
 
 #echo "Building Test App for A4"
 #g++ -o testA4 mainA4.cpp common/AddressMapper.cpp 
