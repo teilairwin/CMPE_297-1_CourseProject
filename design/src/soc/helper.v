e364abd6748e85461953c420b259d0202a41bdba
Kaylee <kayleemiller17@gmail.com>
2021-03-28 11:33:29 -0600

    Update helper.v
    Adding initial q in dreg_en

--- design/src/soc/helper.v
+++ design/src/soc/helper.v
@@ -140,6 +140,9 @@ module dreg_en # (parameter WIDTH = 32) (
   input wire [WIDTH-1:0] d,
   output reg [WIDTH-1:0] q
 );
+  initial begin
+    q = 0;
+  end
   always @ (posedge clk, posedge rst) begin
     if (rst) q <= 0;
     else if (en) q <= d;
