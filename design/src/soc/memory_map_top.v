29e8b1ff13873a5630d8900d808ca5750356a8f0
mkl1ghtn1ng <64824197+mkl1ghtn1ng@users.noreply.github.com>
2021-04-03 16:36:42 -0600

    Updated MM tb. And module bug fix for WE.

--- design/src/soc/memory_map_top.v
+++ design/src/soc/memory_map_top.v
@@ -57,15 +57,23 @@ module memory_map_top(
     wire [2:0] select;
     wire out_of_range_error;
     wire validity;
+    wire [5:0] mm_we;
     
     address_map address_map(
         //input
         .input_addr     (input_addr),
         //outputs
         .select         (select),
-        .write_enable   ({fact3_we,fact2_we,fact1_we,fact0_we,intc_we,dm_we}),
+        .write_enable   (mm_we),
         .out_of_range_error (out_of_range_error)
     );
+    
+    assign dm_we = write_enable & mm_we[0];
+    assign intc_we = write_enable & mm_we[1];
+    assign fact0_we = write_enable & mm_we[2];
+    assign fact1_we = write_enable & mm_we[3];
+    assign fact2_we = write_enable & mm_we[4];
+    assign fact3_we = write_enable & mm_we[5];
     
     // use select to mux to the right address and data lines
     mux8 #32 data_mux(
--- ver/testbenches/tb_mm_top.v
+++ ver/testbenches/tb_mm_top.v
@@ -16,6 +16,160 @@ module tb_mm_top();
     wire [31:0] dev_addr [5:0];
     wire [31:0] dev_wdata [5:0];
     
+    //Test Variables
+    integer ii = 0;
+    integer errorCount = 0;
+    reg [31:0] dev_baseAddr [5:0];
+    
+    //-- Cycle the simulation 
+    task tick; 
+    begin
+        #10;  
+    end endtask
+    //-- Reset the simulation
+    task reset;
+    begin 
+        write_enable = 0;
+        input_addr = 32'b0;
+        input_data = 32'b0;
+        for(ii=0; ii<6; ii=ii+1) begin
+            dev_rdata[ii] = 32'b0;
+        end
+        dev_baseAddr[0] = 32'h0000_0000;
+        dev_baseAddr[1] = 32'h0000_2000;
+        dev_baseAddr[2] = 32'h0000_3000;
+        dev_baseAddr[3] = 32'h0000_4000;
+        dev_baseAddr[4] = 32'h0000_5000;
+        dev_baseAddr[5] = 32'h0000_6000;
+        tick;
+    end endtask
+    
+    //-- Check a 1b value
+    task check1;
+    input val;
+    input exp;
+    begin
+        if(val != exp) begin
+            $display("[%d]TestComp value[0x%h] != expected[0x%h]!",$time,val,exp);
+            errorCount = errorCount +1;
+        end
+    end endtask
+    //-- Check a 1b value
+    task check3;
+        input [2:0] val;
+        input [2:0] exp;
+        begin
+            if(val != exp) begin
+                $display("[%d]TestComp value[0x%h] != expected[0x%h]!",$time,val,exp);
+                errorCount = errorCount +1;
+            end
+        end endtask
+    //-- Check a 32b value
+    task check32;
+    input [31:0] val;
+    input [31:0] exp;
+    begin
+        if(val != exp) begin
+            $display("[%d]TestComp value[0x%h] != expected[0x%h]!",$time,val,exp);
+            errorCount = errorCount +1;
+        end
+    end endtask
+    
+    task test_dev_access;
+    input [2:0] devN;
+    begin
+        $display("[%d]Checking Dev Access For: %d",$time,devN);
+        reset;
+        
+        //Pre-Check
+        $display("\tPRE_CONDITIONS");
+        //Check the dev connections
+        $display("\t\tChecking DevWE:0x%h",dev_we[devN]);
+        check1(dev_we[devN], 1'b0);
+        $display("\t\tChecking DevADDR:0x%h",dev_addr[devN]);
+        check32(dev_addr[devN], 32'b0);
+        $display("\t\tChecking DevWData:0x%h",dev_wdata[devN]);
+        check32(dev_wdata[devN], 32'b0);
+        //Check the host connections
+        $display("\t\tChecking MmOutData:0x%h",output_data);
+        check32(output_data, 32'b0);
+        $display("\t\tChecking MmDataValid:0x%h",data_valid);
+        check1(data_valid, 1'b1);
+        
+        //Check the base address select & read
+        $display("\tBASE-ADDR-READ"); 
+        input_addr = dev_baseAddr[devN];
+        dev_rdata[devN] = $urandom();
+        tick;
+        $display("\t\tChecking BaseAddr Select:0x%h",DUT.select);
+        check3(DUT.select,devN);
+        $display("\t\tChecking DevAddr Exp:0x%h Got:0x%h",input_addr,dev_addr[devN]);
+        check32(input_addr,dev_addr[devN]);
+        $display("\t\tChecking BaseAddrRead Exp:0x%h Got:0x%h",dev_rdata[devN],output_data);
+        check32(dev_rdata[devN],output_data);
+        check1(dev_we[devN], 1'b0);
+        
+        //Check the address select & read 
+        $display("\tOFFSET-READ");
+        input_addr = dev_baseAddr[devN]+32'h8;
+        dev_rdata[devN] = $urandom();
+        tick;
+        $display("\t\tChecking Addr Select:0x%h",DUT.select);
+        check3(DUT.select,devN);
+        $display("\t\tChecking DevAddr Exp:0x%h Got:0x%h",input_addr,dev_addr[devN]);
+        check32(input_addr,dev_addr[devN]);
+        $display("\t\tChecking BaseAddrRead Exp:0x%h Got:0x%h",dev_rdata[devN],output_data);
+        check32(dev_rdata[devN],output_data);
+        check1(dev_we[devN], 1'b0);
+        
+        //Write base address
+        $display("\tBASE-ADDR-WRITE");
+        input_addr = dev_baseAddr[devN];
+        dev_rdata[devN] = $urandom();
+        input_data = $urandom();
+        write_enable = 1'b1;
+        tick;
+        $display("\t\tChecking Addr Select:0x%h",DUT.select);
+        check3(DUT.select,devN);
+        $display("\t\tChecking DevAddr Exp:0x%h Got:0x%h",input_addr,dev_addr[devN]);
+        check32(input_addr,dev_addr[devN]);
+        $display("\t\tChecking WriteData Exp:0x%h Got:0x%h",dev_wdata[devN],input_data);
+        check32(dev_wdata[devN],input_data);
+        $display("\t\tChecking WE:0x%h",dev_we[devN]);
+        check1(dev_we[devN], 1'b1);
+        $display("\t\tchecking Validity:0x%h",data_valid);
+        check1(data_valid, 1'b1);
+        
+        //Write offset address
+        $display("\tOFFSET-WRITE");
+        input_addr = dev_baseAddr[devN]+32'hC;
+        dev_rdata[devN] = $urandom();
+        input_data = $urandom();
+        write_enable = 1'b1;
+        tick;
+        $display("\t\tChecking Addr Select:0x%h",DUT.select);
+        check3(DUT.select,devN);
+        $display("\t\tChecking DevAddr Exp:0x%h Got:0x%h",input_addr,dev_addr[devN]);
+        check32(input_addr,dev_addr[devN]);
+        $display("\t\tChecking WriteData Exp:0x%h Got:0x%h",dev_wdata[devN],input_data);
+        check32(dev_wdata[devN],input_data);
+        $display("\t\tChecking WE:0x%h",dev_we[devN]);
+        check1(dev_we[devN], 1'b1);
+        $display("\t\tchecking Validity:0x%h",data_valid);
+        check1(data_valid, 1'b1);    
+        
+    end endtask
+    
+    //-- Exit the simulation
+    task exit;
+    begin
+        $display("\nExit");
+        $display("[%d]Leaving simulation...",$time);
+        $display("[%d]Total Errors: %d",$time,errorCount);
+        $finish;
+    end
+    endtask
+    
     //Instantiate DUT
     memory_map_top DUT(
         //MIPS Processor connections
@@ -60,50 +214,16 @@ module tb_mm_top();
     //    #5 clk = ~clk;
     //end
     initial begin
-        clk = 0;
-        write_enable <= 1'b0;
+        reset;
         
-        // test case 1: send data to the DM
-        #20
-        input_addr <= 32'h00000000;
-        input_data <= 32'h0000000A;
-        #20
-        write_enable <= 1'b1;
-        #20
-        input_data <= 32'h0; 
-        #20
-        write_enable <= 1'b0;
-        #20
-        // test case 2: send data to the interrupt controller
-        input_addr <= 32'h00010000;
-        input_data <= 32'h0000000B;
-        #20
-        write_enable <= 1'b1;;
-        #20
-        input_data <= 32'h0; 
-        #20
-        write_enable <= 1'b0;
-        #20
-        // test case 3: send data to factorial unit 0
-        input_addr <= 32'h00020000;
-        input_data <= 32'h0000000C;
-        #20
-        write_enable <= 1'b1;
-        #20
-        input_data <= 32'h0; 
-        #20
-        write_enable <= 1'b0;
-    // test case 3: send data to factorial unit 0
-        input_addr <= 32'h00030000;
-        input_data <= 32'h0000000D;
-        #20
-        write_enable <= 1'b1;
-        #20
-        input_data <= 32'h0; 
-        #20
-        write_enable <= 1'b0;    
-        $finish;
+        test_dev_access(3'd0);
+        test_dev_access(3'd1);
+        test_dev_access(3'd2);
+        test_dev_access(3'd3);
+        test_dev_access(3'd4);
+        test_dev_access(3'd5);
         
+        exit;
     end
 
 endmodule
